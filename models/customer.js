0673422514d53cbbaf250225ad0b8d059a379287
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-01-21 14:29:59 +0200

    first commit

---
+++ models/customer.js
@@ -0,0 +1,30 @@
+const Joi = require('joi')
+const mongoose = require('mongoose')
+
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 255
+    },
+    mobile: {
+        type: String,
+        required: true,
+        minlength : 3,
+        maxlength: 100
+    },
+    isGold: {
+        type: Boolean,
+        default: false
+    }
+}))
+
+
+function validateCustomer(Customer){
+    const Schema Joi.object({
+        name: Joi.String().min(3).max(255).require(),
+        mobile: Joi.String().min(3).max(15).require(),
+        isGold: Joi.Boolean()
+    })
+}
