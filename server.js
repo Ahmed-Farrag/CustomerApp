7510e9ed81871ed3cee10905cf0be8b636bd7795
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-01-25 22:05:32 +0200

    vercel file

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const customers = require("./routes/customer");
-const fakeData = require('./routes/faker');
-const methodOverride = require('method-override')
-const search = require('./routes/search')
+// const customers = require("./routes/customer");
+// const fakeData = require('./routes/faker');
+// const methodOverride = require('method-override')
+// const search = require('./routes/search')
 
 // connect to db
 mongoose
@@ -22,25 +22,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.use(methodOverride(function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
-  }
-}))
+// app.use(methodOverride(function(req, res){
+//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//     // look in urlencoded POST bodies and delete it
+//     var method = req.body._method
+//     delete req.body._method
+//     return method
+//   }
+// }))
 
-app.set("view engine", "ejs");
-app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
-app.use("/public", express.static(__dirname + "/public"));
+// app.set("view engine", "ejs");
+// app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+// app.use("/public", express.static(__dirname + "/public"));
 
-app.use("/api/customer", customers);
-app.use('/api/fake', fakeData);
-app.use('/api/search', search);
+// app.use("/api/customer", customers);
+// app.use('/api/fake', fakeData);
+// app.use('/api/search', search);
 
 
-
+app.get('/test', (req,res)=>{
+  res.json({msg: 'hello a'})
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
