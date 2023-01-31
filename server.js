564d3860291a33eab7996e31bce6e3b5e04b0256
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-01-31 15:50:19 +0200

    make it berrer

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 app.use("/public", express.static(__dirname + "/public"));
 
 app.use("/", customers);
-app.use("/api/customer", customers);
+// app.use("/api/customer", customers);
 app.use('/api/fake', fakeData);
 app.use('/api/search', search);
 
