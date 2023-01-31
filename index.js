66f1265d751ede114efc3e40b5fcfbc083a95603
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-01-31 22:34:38 +0200

    aploded

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 
 const customers = require("./routes/customer");
-const fakeData = require('./routes/faker');
-const methodOverride = require('method-override')
+// const fakeData = require('./routes/faker');
+// const methodOverride = require('method-override')
 const search = require('./routes/search')
 
 // connect to db
@@ -22,14 +22,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.use(methodOverride(function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
-  }
-}))
+// app.use(methodOverride(function(req, res){
+//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//     // look in urlencoded POST bodies and delete it
+//     var method = req.body._method
+//     delete req.body._method
+//     return method
+//   }
+// }))
 
 app.set("view engine", "ejs");
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
@@ -37,7 +37,7 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", customers);
 app.use("/api/customer", customers);
-app.use('/api/fake', fakeData);
+// app.use('/api/fake', fakeData);
 app.use('/api/search', search);
 
 
